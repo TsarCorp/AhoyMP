d12eb45e9b105e7147a55c8ca34ffeed8c781d3e
TsarCorp <tariq@tariqsarwar.net>
2017-03-23 21:38:56 +0000

    Imported Skybox and Assets
    Skybox from asset store and assets from course

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -4,17 +4,20 @@ using UnityStandardAssets.CrossPlatformInput;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Player : NetworkBehaviour {
+public class Player : NetworkBehaviour
+{
 
     private Vector3 inputValue;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         if (!isLocalPlayer)
         {
@@ -28,14 +31,14 @@ public class Player : NetworkBehaviour {
         transform.Translate(inputValue);
 
 
-        
+
     }
 
     public override void OnStartLocalPlayer()
     {
         GetComponentInChildren<Camera>().enabled = true;
         //Debug.Log(Time.timeSinceLevelLoad + " Starting Local Player Camera: ");
-        
+
     }
 
 
